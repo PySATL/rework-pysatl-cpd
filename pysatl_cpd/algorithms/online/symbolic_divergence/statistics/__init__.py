b08d4e1e84054f4003f34c22fe46981f8839125a
julia <julia.b.omsk@gmail.com>
2026-06-21 18:31:39 +0300

    feat(symbolic_divergence): add LogScaledDivergenceStatistic
    Add a log-damped change-point statistic (scale * n / log(n+1) * D)

--- pysatl_cpd/algorithms/online/symbolic_divergence/statistics/__init__.py
+++ pysatl_cpd/algorithms/online/symbolic_divergence/statistics/__init__.py
@@ -17,6 +17,9 @@ change-point function.
   divergence unchanged (default).
 - ``ScaledDivergenceStatistic`` -- ``scale * sample_size * divergence``; with
   ``scale = 2.0`` this reproduces the ``2 n D`` statistic.
+- ``LogScaledDivergenceStatistic`` -- ``scale * sample_size /
+  log(sample_size + 1) * divergence``; a logarithmically damped variant of the
+  linear scaling.
 """
 
 __author__ = "Yulia Burmistrova"
@@ -25,7 +28,13 @@ __license__ = "SPDX-License-Identifier: MIT"
 
 
 from pysatl_cpd.algorithms.online.symbolic_divergence.statistics.base import IChangePointStatistic
+from pysatl_cpd.algorithms.online.symbolic_divergence.statistics.log_scaled import LogScaledDivergenceStatistic
 from pysatl_cpd.algorithms.online.symbolic_divergence.statistics.raw import RawDivergenceStatistic
 from pysatl_cpd.algorithms.online.symbolic_divergence.statistics.scaled import ScaledDivergenceStatistic
 
-__all__ = ["IChangePointStatistic", "RawDivergenceStatistic", "ScaledDivergenceStatistic"]
+__all__ = [
+    "IChangePointStatistic",
+    "LogScaledDivergenceStatistic",
+    "RawDivergenceStatistic",
+    "ScaledDivergenceStatistic",
+]
